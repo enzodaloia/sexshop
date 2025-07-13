5c9316b4d1177a8ca008d94604523ec6ffe9a95d
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2025-07-13 15:32:01 +0200

    v2

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,6 +22,39 @@ final class IndexController extends AbstractController
     {
         return $this->render('index/articles.html.twig', [
             
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'app_one_article', requirements: ['id' => '\d+'])]
+    public function articleByOffer(int $id): Response
+    {
+        $products = [
+            1 => [
+                'id' => 1,
+                'img' => 'assets/img/photo_noel_gode.png',
+            ],
+            2 => [
+                'id' => 2,
+                'img' => 'assets/img/vibro-violet.jpg',
+            ],
+            3 => [
+                'id' => 3,
+                'img' => 'assets/img/gode-confetti.webp',
+            ],
+            4 => [
+                'id' => 4,
+                'img' => 'assets/img/gode-ventouse.jpg',
+            ],
+        ];
+        
+        if (!isset($products[$id])) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+        
+        $product = $products[$id];
+        
+        return $this->render('index/article.html.twig', [
+            'product' => $product
         ]);
     }
 }
